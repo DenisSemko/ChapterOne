e0f25abd6c741354c6b7fc18d3197f05a05c0b24
Denis Semko <dench327@gmail.com>
2022-06-14 18:43:45 +0300

    Change GMAIL Settings

--- BLL/Services/Concrete/BookPaymentService.cs
+++ BLL/Services/Concrete/BookPaymentService.cs
@@ -120,7 +120,7 @@ namespace BLL.Services.Concrete
 
             var client = new SmtpClient();
             await client.ConnectAsync(_emailConfig.SmtpServer, _emailConfig.Port, true);
-            client.AuthenticationMechanisms.Remove("XOAUTH2");
+            //client.AuthenticationMechanisms.Remove("XOAUTH2");
             await client.AuthenticateAsync(_emailConfig.UserName, _emailConfig.Password);
             await client.SendAsync(emailMessage);
             await client.DisconnectAsync(true);
